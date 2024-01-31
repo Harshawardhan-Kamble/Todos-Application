99d989ffa4f0aa1d5373525f77159c262631f80d
Harshawardhan Kamble <kambleharsh08@outlook.com>
2024-01-31 15:42:37 +0530

    backend completed

--- backend/db.js
+++ backend/db.js
@@ -1,5 +1,6 @@
 const mongoose=require("mongoose")
-mongoose.connect('mongodb+srv://k_harsh08:9HsqoPdVd4WI63Fc@cluster0.cyiljaj.mongodb.net/todos');
+const DB_URL=process.env.DB_URL;
+mongoose.connect(DB_URL);
 
 const Todo = mongoose.model('Todo', { title: String,
     description:String,
--- backend/index.js
+++ backend/index.js
@@ -1,40 +1,56 @@
-const {createTodo,updateTodo}=require ("./types")
-const express=require("express")
-const app= express()
-app.use(express.json())
-app.post("/todos",(req,res)=>{ 
-    // Zod validation
-    const createPayload=req.body;
-    const parsePayload=createTodo.safeParse(createPayload)
-    if(!parsePayload.sucess){
-        res.status(411).json({msg:"You have entered wrong input"})
-        return;
+const dotenv = require("dotenv");
+dotenv.config();
+const { Todo } = require("./db");
+const { createTodo, updateTodo } = require("./types");
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.post("/todo", async (req, res) => {
+  // Zod validation
+  const createPayload = req.body;
+  const parsePayload = createTodo.safeParse(createPayload);
+
+  if (!parsePayload.success) {
+    res.status(411).json({ msg: "You have entered wrong input" });
+    return;
+  }
+  // Mongodb
+  await Todo.create({
+    title: createPayload.title,
+    description: createPayload.description,
+    completed: false,
+  });
+  res.json({
+    msg: "Todo created",
+  });
+});
+app.get("/todos", async (req, res) => {
+  const todos = await Todo.find({});
+  res.json({
+    todos,
+  });
+});
+app.put("/completed", async (req, res) => {
+  // Zod validation
+  const updatePayload = req.body;
+  const parsePayload = updateTodo.safeParse(updatePayload);
+  if (!parsePayload.success) {
+    res.status(411).json({ msg: "Something went wrong" });
+    return;
+  }
+  await Todo.updateOne(
+    {
+      _id: updatePayload.id,
+    },
+    {
+      completed: true,
     }
-    else{
-        // Mongodb
-    }
-
-
-})
-app.get("/todo",(req,res)=>{
-
-
-})
-app.put("/completed",(req,res)=>{
-     // Zod validation
-    const createPayload=req.body;
-    const parsePayload=updateTodo.safeParse(createPayload);
-    if(!parsePayload.success){
-        res.status(411).json({msg:"Something went wrong"})
-        return;
-    }
-    else{
-        // Mongodb
-        
-    }
-
-
-})
-app.listen(3000,()=>{
-    console.log("Server Started on host 3000")
-})
+  );
+  res.json({
+    msg: "Todo mark Completed",
+  });
+  // Mongodb
+});
+app.listen(3000, () => {
+  console.log("Server Started on host 3000");
+});
